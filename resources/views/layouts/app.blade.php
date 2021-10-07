7bd4068037f00012b61e08c2cadb8508b9ec5aca
Salahuddin <sholahudinbest@gmail.com>
2021-10-07 16:01:37 +0800

    - Install tailwind - Create form
    - Install tailwind
    - Create form

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rumah Kreatif Khansa</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('css/theme.css') }}" rel="stylesheet">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
